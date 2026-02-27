f23db955b7ff36010a40676856843fcadd25a1e3
Akanksha verma <vermaakanksha658@gmail.com>
2026-02-27 20:34:17 +0530

    Delete Config directory

--- Config/database.js
+++
@@ -1,7 +0,0 @@
-const mongoose=require("mongoose");
-
-async function connectDB(){
-    await mongoose.connect("mongodb://localhost:27017/bcrypassdb")
-    console.log("database connect")
-}
-module.exports=connectDB;
